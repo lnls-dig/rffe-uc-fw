a6bfcc32fc9da1b75f8b3976f8f9fefa8dcc7fb3
Henrique Silva <henrique.silva@lnls.br>
2018-06-18 17:31:00 -0300

    [src/main.cpp] Increase CLI's thread stack size

--- src/main.cpp
+++ src/main.cpp
@@ -112,7 +112,7 @@ bsmp_server_t *bsmp;
 // Threads
 Thread Temp_Control_thread(osPriorityNormal, 1200, NULL, "TEMP");
 Thread Attenuators_thread(osPriorityNormal, 800, NULL, "ATT");
-Thread CLI_Proccess_Thread(osPriorityNormal, 1024, NULL, "CLI");
+Thread CLI_Proccess_Thread(osPriorityNormal, 1200, NULL, "CLI");
 
 // Hardware Initialization - MBED
 

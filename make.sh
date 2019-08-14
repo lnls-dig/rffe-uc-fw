1bed6f64e469a1aa12f002d69715af8adcd58b2b
Augusto Fraga Giachero <augustofg96@gmail.com>
2019-08-14 12:08:39 -0300

    Script to automate building and flashing
    This is a simple shell script to configure, build and flash the RFFE
    application + Nuttx. It might be a good idea to turn it into a
    Makefile or cmake script in the future.

---
+++ make.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+cmd="$1"
+
+if [ -z "$JOBS" ]; then
+	JOBS=1
+fi
+
+if test "$cmd" = "configure"; then
+	cd apps/
+	rm -f external
+	ln -s ../rffe-app external
+	cd ..
+	cd nuttx/
+	./tools/configure.sh ../rffe-board/rffe
+	cd ..
+elif test "$cmd" = "build"; then
+	cd nuttx/
+	if [ ! -e .config ]; then
+		echo "Error: Build not configured yet."
+		echo "Please run '$0 configure' before building."
+		exit 1
+	fi
+    make -j ${JOBS}
+	cd ..
+elif test "$cmd" = "clean"; then
+	cd nuttx/
+	make distclean
+	cd ..
+	rm apps/external
+elif test "$cmd" = "flash"; then
+	openocd -f scripts/openocd/lpc17-cmsis.cfg -c "program nuttx/nuttx; reset; shutdown"
+fi

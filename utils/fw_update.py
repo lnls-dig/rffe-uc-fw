3b80b59584e72b808268a8dbac9849f9ea0e801c
Augusto Fraga Giachero <augustofg96@gmail.com>
2020-02-14 15:41:36 -0300

    Add bootloader option in fw_update.py [utils]
    Now it is possible to update the bootloader via the fw_update.py
    script.

--- utils/fw_update.py
+++ utils/fw_update.py
@@ -8,8 +8,16 @@ try:
     fw_file = sys.argv[2]
     version = sys.argv[3]
 except:
-    print("Usage: " + sys.argv[0] + " ip firmware_file version")
+    print("Usage: " + sys.argv[0] + " ip firmware_file version [bootloader]")
     exit(1)
+
+boot = False
+try:
+    boot_arg = sys.argv[4]
+    if (boot_arg == "bootloader"):
+        boot = True
+except:
+    pass
 
 print("Connecting to " + ip_addr + " ...")
 try:
@@ -19,5 +27,5 @@ except:
     exit(1)
 
 print("Connection established, writing new firmware...")
-rffe.reprogram(fw_file, version)
+rffe.reprogram(fw_file, version, boot)
 print("Finished!")

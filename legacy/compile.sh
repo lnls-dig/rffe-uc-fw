8ad3966d0b0f2b166f5818ab61627e76fd9ce92c
Augusto Fraga Giachero <augustofg96@gmail.com>
2020-02-14 16:05:34 -0300

    Remove legacy code
    No longer necessary, we don't need the legacy bootloader anymore. Good
    bye mbed-os.

--- legacy/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-mbed compile -j 4 -t GCC_ARM -m LPC1768 --source bootloader/ --source mbed-os/ --build BUILD/bootloader
-mbed compile -j 4 -t GCC_ARM -m LPC1768 --source app/ --source mbed-os/ -N rffe-uc-firmware --build BUILD/rffe-uc-firmware
